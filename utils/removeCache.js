25f2e0a77f4c45caafe19989ce7ddf7a0c2edd0f
DESKTOP-736CVB7\Technisia <balochdanish2020@gmail.com>
2023-11-14 12:41:16 +0500

    updated

--- utils/removeCache.js
+++ utils/removeCache.js
@@ -1,3 +1,5 @@
+import { revalidatePath } from "next/cache";
+
 export const forceRevalidate = (request) => {
   const path = request.nextUrl.searchParams.get("path") || "/";
   revalidatePath(path);
