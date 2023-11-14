b23b97880881a4babafd162da3b495bb4fef28ee
DESKTOP-736CVB7\Technisia <balochdanish2020@gmail.com>
2023-11-14 12:28:53 +0500

    updated

--- app/api/project/allrecords/route.js
+++ app/api/project/allrecords/route.js
@@ -1,7 +1,15 @@
 import Project from "@/models/project";
 import { connectToDB } from "@/utils/database";
+import { revalidatePath } from "next/cache";
+import { NextRequest } from "next/server";
 
+export const forceRevalidate = (request) => {
+  const path = request.nextUrl.searchParams.get("path") || "/";
+  revalidatePath(path);
+};
 export const GET = async (request) => {
+  forceRevalidate(request);
+
   try {
     await connectToDB();
 
@@ -20,3 +28,5 @@ export const GET = async (request) => {
     return new Response("Internal Server Error", { status: 404 });
   }
 };
+
+export const revalidate = 0;
